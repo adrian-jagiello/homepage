d0d22cb047d613fbe40c87ec5dee40ec6b9cd3d5
Adrian Jagiełło <adrian.jagiello@icloud.com>
2020-05-24 16:08:22 +0200

    welcome function new background gradient color need to fix index photo position

--- main.js
+++ main.js
@@ -1,8 +1,16 @@
+
 const firstName = 'Adrian' ;
 const lastName = 'Jagiełło' ;
 const age = 22 ;
 
-console.log(firstName) ;
-console.log(lastName) ;
-console.log(age) ;
-console.log(`Cieść, nazywam się ${firstName} ${lastName} i mam ${age} lata`)
+const heading = document.querySelector('.page-header--js') ;
+
+heading.innerHTML = `Cieść, nazywam się ${firstName} ${lastName} i mam ${age} lata.` ;
+
+function greet(age, firstName){
+    console.log(
+        `Cieść, nazywam się ${firstName} i mam ${age} lata.`
+    ) ;
+}
+
+greet(age, firstName)
