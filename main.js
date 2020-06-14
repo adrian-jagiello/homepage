9a2dd2c58483dcc7dd0b9883428ea7fd0e81449a
Adrian Jagiełło <adrian.jagiello@icloud.com>
2020-06-14 20:37:51 +0200

    media fix change bem

--- main.js
+++ main.js
@@ -1,16 +1,13 @@
+const firstName = "Adrian";
+const lastName = "Jagiełło";
+const age = 22;
 
-const firstName = 'Adrian' ;
-const lastName = 'Jagiełło' ;
-const age = 22 ;
+const heading = document.querySelector(".page-header--js");
 
-const heading = document.querySelector('.page-header--js') ;
+heading.innerHTML = `Cieść, nazywam się ${firstName} ${lastName} i mam ${age} lata.`;
 
-heading.innerHTML = `Cieść, nazywam się ${firstName} ${lastName} i mam ${age} lata.` ;
-
-function greet(age, firstName){
-    console.log(
-        `Cieść, nazywam się ${firstName} i mam ${age} lata.`
-    ) ;
+function greet(age, firstName) {
+  console.log(`Cieść, nazywam się ${firstName} i mam ${age} lata.`);
 }
 
-greet(age, firstName)
+greet(age, firstName);
