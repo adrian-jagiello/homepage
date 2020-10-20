b228e260e35d68af885f686427d73f44cc074b0f
Adrian Jagiełło <adrian.jagiello@icloud.com>
2020-10-20 15:01:34 +0200

    burger menu fix

--- main.js
+++ main.js
@@ -4,7 +4,6 @@ const age = 22;
 
 const heading = document.querySelector(".page-header--js");
 
-heading.innerHTML = `Cieść, nazywam się ${firstName} ${lastName} i mam ${age} lata.`;
 
 function greet(age, firstName) {
   console.log(`Cieść, nazywam się ${firstName} i mam ${age} lata.`);
