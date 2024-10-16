04a324f3bb111e40c6084262055d4cfc36a16b18
Adrian Jagiełło <64840455+adrian-jagiello@users.noreply.github.com>
2024-10-16 21:00:04 +0200

    The main.js file was added, along with a welcome script visible in the console.

---
+++ main.js
@@ -0,0 +1,10 @@
+
+const first_name = "Adrian";
+const age = "26";
+
+console.log(first_name);
+console.log(age);
+
+console.log(
+  `Nazywam się ${first_name} i mam ${age} lat, i chciałbym przybliżyć tobie świat kawy speciality.`
+);
